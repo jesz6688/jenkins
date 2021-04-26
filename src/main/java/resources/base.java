f3df6cbf1930cec014a2e0a013a75aa41be894e6
visheshRaaj23 <visheshudupa@gmail.com>
2021-04-26 16:32:05 +0530

    My First Commit

---
+++ src/main/java/resources/base.java
@@ -0,0 +1,63 @@
+package resources;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+public class base {
+
+	public WebDriver driver;
+	public Properties prop;
+
+	public WebDriver initializeDriver() throws IOException {
+
+		prop = new Properties();
+		FileInputStream fis = new FileInputStream("./src/main/java/resources/data.properties");
+
+		prop.load(fis);
+		//String browserName = prop.getProperty("browser");
+		String browserName = System.getProperty("browser");
+		
+		System.out.println(browserName);
+//mvn clean install test -Dbrowser=chrome
+		if (browserName.equalsIgnoreCase("chrome")) {
+			System.setProperty("webdriver.chrome.driver", "./server/chromedriver.exe");
+			driver = new ChromeDriver();
+			// execute in chrome driver
+
+		} else if (browserName.equalsIgnoreCase("edge")) {
+			System.setProperty("webdriver.edge.driver", "./server/msedgedriver.exe");
+			driver = new EdgeDriver();
+			// firefox code
+		} else if (browserName.equalsIgnoreCase("IE")) {
+			System.setProperty("webdriver.ie.driver", "./server/IEDriverServer.exe");
+			driver = new InternetExplorerDriver();
+			// IE code
+		}
+
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		return driver;
+
+	}
+
+	public String getScreenShotPath(String testCaseName, WebDriver driver) throws IOException {
+		TakesScreenshot ts = (TakesScreenshot) driver;
+		File source = ts.getScreenshotAs(OutputType.FILE);
+		String destinationFile = System.getProperty("user.dir") + "\\reports\\" + testCaseName + ".png";
+		FileUtils.copyFile(source, new File(destinationFile));
+		return destinationFile;
+
+	}
+
+}
